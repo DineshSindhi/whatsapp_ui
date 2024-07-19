48a508e47891ae888100147f7cf6a73a376e665b
Dinesh <dineshchhangani9928@gmail.com>
2024-07-19 11:24:36 +0530

    whatsapp 3 ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:pages/status.dart';
-import 'package:pages/tip_calculator.dart';
-import 'package:pages/wathspp.dart';
-import 'package:pages/whatsappcall.dart';
+import 'package:pages/wathsapp.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -26,111 +24,6 @@ class MyApp extends StatelessWidget {
       ),
       home: Whatsapp(),
     );
-  }
-}
-class MyHomePage extends StatefulWidget{
-
-  @override
-  State<StatefulWidget> createState() {
-    return HomePage();
-  }
-}
-class  HomePage extends State<MyHomePage>{
-  TextEditingController weController =TextEditingController();
-  TextEditingController eController =TextEditingController();
-  TextEditingController hiController =TextEditingController();
-  String mImages="";
-
-  dynamic result='';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Page'),
-        backgroundColor: Colors.cyan,
-      ),
-      body: Container(
-        color: Colors.orange.shade100,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: weController,
-                decoration: InputDecoration(
-                  label: Text('Enter your weight'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: eController,
-                decoration: InputDecoration(
-                  label: Text('height in feets'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: hiController,
-                decoration: InputDecoration(
-                  label: Text('height in inch'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-              ),
-            ),
-            ElevatedButton(onPressed: (){
-              if(weController.text.isNotEmpty&&eController.text.isNotEmpty&&hiController.text.isNotEmpty) {
-                int no1 = int.parse(weController.text);
-                int no2 = int.parse(eController.text);
-                int no3 = int.parse(hiController.text);
-                num sum=0;
-                num cm=(no2*12)+no3;
-                num met=cm*2.54;
-                num height=met/100;
-                sum= no1/pow(height, 2);
-                if(sum>25){
-                  result= 'BMI is ${sum.toStringAsFixed(2)}\n''High';
-                  mImages='assets/images/we.png';
-                } else if(sum<18){
-                  result= 'BMI is ${sum.toStringAsFixed(2)}\nlow';
-                  mImages='assets/images/patla.png';
-                }else{
-                  result = 'BMI is ${sum.toStringAsFixed(2)}\nperfect';
-                  mImages='assets/images/fit.png';
-
-                }
-                setState(() {
-
-                });
-
-              }else{
-                setState(() {
-                  result= 'Please fill the blanks';
-                  mImages="";
-                });
-              }
-            }, child: Text('BMI')),
-            Text(result,style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
-            mImages!="" ? Image.asset(mImages,height: 100,width: 100,):Container()
-          ],
-        ),
-      ),
-    );
-
   }
 }
 
